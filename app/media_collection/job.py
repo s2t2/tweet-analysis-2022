7c9a6f1776e110c219b82af6fb92d5d80c7deaed
MJ Rossetti <s2t2@users.noreply.github.com>
2022-07-31 11:53:58 -0400

    Upload images to cloud storage

--- app/media_collection/job.py
+++ app/media_collection/job.py
@@ -5,27 +5,23 @@
 import os
 #from uuid import uuid4
 
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import requests
 
 from app import seek_confirmation
+from app.cloud_storage import GoogleCloudStorageService
 from app.tweet_collection.job import STORAGE_MODE #, Job
 from app.tweet_collection.db import CollectionDatabase
 #from app.tweet_collection.bq import BigQueryDatabase
 
-#load_dotenv()
+load_dotenv()
 
-#MEDIA_STORAGE_MODE = os.getenv("MEDIA_STORAGE_MODE", default="local")
+MEDIA_STORAGE_MODE = os.getenv("MEDIA_STORAGE_MODE", default="local")
+
 MEDIA_DIR = os.path.join(os.path.dirname(__file__), "..", "..", "media")
 
-#class MediaStorage:
-#    def __init__(self):
-#        pass
-
-
 class Job:
-    #def __init__(self, storage_mode=STORAGE_MODE, media_storage_mode=MEDIA_STORAGE_MODE, media_limit=None):
-    def __init__(self, storage_mode=STORAGE_MODE, media_limit=None):
+    def __init__(self, storage_mode=STORAGE_MODE, media_storage_mode=MEDIA_STORAGE_MODE, media_limit=None):
         self.storage_mode = storage_mode
         if self.storage_mode == "sqlite":
             self.db = CollectionDatabase(destructive=False)
@@ -34,17 +30,19 @@ class Job:
         else:
             raise AttributeError("oops wrong storage mode")
 
-        #self.media_storage_mode = media_storage_mode
-        #if self.media_storage_mode == "local":
-        #    self.media_store = MediaStorage()
-        ##elif self.storage_mode == "gcs":
-        ##    self.media_store = GoogleCloudStorageService()
-        #else:
-        #    raise AttributeError("oops wrong media storage mode")
+        self.media_storage_mode = media_storage_mode.lower()
+        if self.media_storage_mode == "local":
+            self.media_store = None
+        elif self.media_storage_mode in ["gcs", "remote", "cloud_storage"]:
+            self.media_store = GoogleCloudStorageService()
+        # maybe consider AWS if you like that kind of thing
+        else:
+            raise AttributeError("oops wrong media storage mode")
 
         print("------------------")
         print("MEDIA COLLECTION JOB...")
 
+        print()
         seek_confirmation()
 
 
@@ -53,31 +51,41 @@ class Job:
         with open(local_filepath, "wb") as media_file:
             media_file.write(response.content)
 
+
     def perform(self):
         for row in self.db.get_downloadable_media():
             media_key = row["media_key"]
-            #media_type = row["media_type"]
+            media_type = row["media_type"]
             media_url = row["media_url"]
             preview_url = row["preview_image_url"]
 
-            print("DOWNLOADING...")
+            print(f"{media_type.upper()} {media_key}...")
 
-            # DOWNLOAD MEDIA
+            # MEDIA
             if media_url:
                 #filename = f"{media_type}_{media_key}_media.jpg"
                 filename = f"{media_key}_media.jpg"
-                print("... ", filename)
+                print("... DOWNLOADING ...", filename)
                 local_filepath = os.path.join(MEDIA_DIR, filename)
                 self.download_media(url=media_url, local_filepath=local_filepath)
 
-            # DOWNLOAD PREVIEW
+                if self.media_store:
+                    remote_filepath = os.path.join("media", filename)
+                    print("... UPLOADING ...", remote_filepath)
+                    self.media_store.upload(local_filepath=local_filepath, remote_filepath=remote_filepath)
+
+            # PREVIEW
             if preview_url:
                 #filename = f"{media_type}_{media_key}_preview.jpg"
                 filename = f"{media_key}_preview.jpg"
-                print("... ", filename)
+                print("... DOWNLOADING ...", filename)
                 local_filepath = os.path.join(MEDIA_DIR, filename)
                 self.download_media(url=preview_url, local_filepath=local_filepath)
 
+                if self.media_store:
+                    remote_filepath = os.path.join("media", filename)
+                    print("... UPLOADING ...", remote_filepath)
+                    self.media_store.upload(local_filepath=local_filepath, remote_filepath=remote_filepath)
 
 
 
